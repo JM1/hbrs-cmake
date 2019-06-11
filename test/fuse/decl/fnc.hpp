0c316dafb5cf8cccf166dd8f73759bda00fb919b
Jakob Meng <jakobmeng@web.de>
2019-06-11 15:26:27 +0200

    Removed unnecessary #pragma once directives

--- test/fuse/decl/fnc.hpp
+++ test/fuse/decl/fnc.hpp
@@ -14,8 +14,6 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#pragma once
-
 #ifndef HBRS_CMAKE_TEST_FUSE_DECL_FNC_HPP
 #define HBRS_CMAKE_TEST_FUSE_DECL_FNC_HPP
 
