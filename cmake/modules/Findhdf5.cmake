ebc9bf942ab61e6087d14ab6b59ea9d74934c9f6
Jakob Meng <jakobmeng@web.de>
2020-11-06 20:46:26 +0100

    Added vim and kate modelines and reformatted files accordingly

--- cmake/modules/Findhdf5.cmake
+++ cmake/modules/Findhdf5.cmake
@@ -1,4 +1,6 @@
 # Copyright (c) 2016-2019 Jakob Meng, <jakobmeng@web.de>
+# vim:set fileformat=unix shiftwidth=4 softtabstop=4 expandtab:
+# kate: end-of-line unix; space-indent on; indent-width 4; remove-trailing-spaces modified;
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
